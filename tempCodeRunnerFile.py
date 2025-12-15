263c52992b3568c7bccc187e80021c4c170d6fda
360sabirrr <360sabirmshaikh@gmail.com>
2025-12-15 16:18:28 +0530

    Add files via upload

---
+++ tempCodeRunnerFile.py
@@ -0,0 +1,7 @@
+def display_menu():
+    print("\nSearch Menu:")
+    print("1. Exact match search")
+    print("2. Search for numbers greater than")
+    print("3. Search for numbers less than")
+    print("4. Search for numbers within a range")
+    print("5. Exit")
